02581aeed375bd29a10f562b91411972375fab8f
georgi-l95 <glazarov95@gmail.com>
2025-09-16 13:04:42 +0300

    edit hardhat config for the example
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>

--- examples/minimal-hardhat/hardhat.config.ts
+++ examples/minimal-hardhat/hardhat.config.ts
@@ -13,18 +13,8 @@ export default {
         "0xYOUR_PRIVATE_KEY",
       ],
     },
-    // Legacy network names are also supported for backward compatibility
-    hedera_testnet: {
-      type: "http",
-      url: "https://testnet.hashio.io/api",
-      accounts: [
-        "0xYOUR_PRIVATE_KEY",
-      ],
-    },
     // mainnet: { url: "https://mainnet.hashio.io/api" },
     // previewnet: { url: "https://previewnet.hashio.io/api" },
-    local: { type: "http", url: "http://localhost:7546", accounts: [
-      "0xb46751179bc8aa9e129d34463e46cd924055112eb30b31637b5081b56ad96129",
-    ] },
+    // local: { url: "http://localhost:7546" },
   },
 } as const;
